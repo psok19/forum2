65a93ee73d8661cf6eb79d728555f2253874657c
Pawel Sokolowski <pawel.sokolowski@hotmail.com>
2020-10-18 02:58:16 +0200

    Test for PostRepository.findAllPostsByTopicId(int id) added.

--- src/test/java/pl/pawelsokolowski/forum2/PostRepositoryTest.java
+++ src/test/java/pl/pawelsokolowski/forum2/PostRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -32,7 +33,7 @@ public class PostRepositoryTest {
 
 
     @Test
-    @Order(1)
+    @Order(10)
     public void shouldIdBeNotNull(){
         topicRepository.save(testTopic);
         postRepository.save(testPost);
@@ -41,14 +42,36 @@ public class PostRepositoryTest {
     }
 
     @Test
-    @Order(2)
+    @Order(20)
     public void shouldFindItem(){
         Optional<Post> item = postRepository.findById(testPost.getPostId());
         Assertions.assertTrue(item.isPresent());
     }
 
     @Test
-    @Order(4)
+    @Order(30)
+    public void shouldFindItemByTopicId(){
+        //given
+        int expectedTopicId = testTopic.getId();
+        int actualTopicId;
+        int expectedNumberOfItems = 1;
+        int actualNumberOfItems;
+
+
+        //when
+        Iterable<Post> actualPosts = postRepository.findAllPostsByTopicId(expectedTopicId);
+        ArrayList<Post> actualPostsList = (ArrayList<Post>)actualPosts;
+        actualTopicId = actualPostsList.get(0).getTopic().getId();
+        actualNumberOfItems = actualPostsList.size();
+
+        //then
+        Assertions.assertEquals(expectedNumberOfItems, actualNumberOfItems);
+        Assertions.assertEquals(expectedTopicId, actualTopicId);
+
+    }
+
+    @Test
+    @Order(40)
     public void shouldDeleteItem(){
         postRepository.delete(testPost);
         topicRepository.delete(testTopic);
@@ -58,7 +81,7 @@ public class PostRepositoryTest {
     }
 
     @Test
-    @Order(5)
+    @Order(50)
     public void shouldNotFindItem(){
         Optional<Post> item = postRepository.findById(testPost.getPostId());
 
